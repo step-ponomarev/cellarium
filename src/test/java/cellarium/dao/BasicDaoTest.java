893e9571e22cdf17696157e3b81d2d92288288f5
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2023-01-18 23:32:26 +0300

    changed

--- src/test/java/cellarium/dao/BasicDaoTest.java
+++ src/test/java/cellarium/dao/BasicDaoTest.java
@@ -90,7 +90,7 @@ public class BasicDaoTest extends ADaoTest {
 
     @Test
     public void testGetAllManyTimes() throws Exception {
-        final int count = 10_000;
+        final int count = 2_000;
 
         try (final Dao<String, Entry<String>> dao = createDao(SIZE_BYTES)) {
             final EntryGeneratorList entries = new EntryGeneratorList(count);
