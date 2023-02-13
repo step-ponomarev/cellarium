37a4169fd096e7860af37302569c11fae8524d82
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2023-02-13 23:58:29 +0300

    changed

--- src/test/java/cellarium/http/AsyncTest.java
+++ src/test/java/cellarium/http/AsyncTest.java
@@ -1,12 +1,11 @@
 package cellarium.http;
 
-import java.io.IOException;
 import org.junit.Test;
 
 public class AsyncTest extends AHttpTest {
     //TODO: Доделать тест
     @Test(timeout = 60000)
-    public void testBadRequestWhenServerShuttingDown() throws IOException, InterruptedException {
+    public void testBadRequestWhenServerShuttingDown() {
         this.stopServer();
     }
 }
