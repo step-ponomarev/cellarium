bb24901ca3a0f5b07961ba812fea7588bbaeed06
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2023-03-26 16:37:13 +0300

    changed

--- src/main/java/cellarium/http/handlers/ExecutorRequestHandler.java
+++ src/main/java/cellarium/http/handlers/ExecutorRequestHandler.java
@@ -10,29 +10,35 @@ import one.nio.http.Response;
 
 public final class ExecutorRequestHandler extends ARequestHandler {
     private final ExecutorService executorService;
-    private final RequestHandler delegate;
+    private final RequestHandler handler;
 
-    public ExecutorRequestHandler(ExecutorService executorService, RequestHandler delegate) {
+    public ExecutorRequestHandler(ExecutorService executorService, RequestHandler handler) {
         this.executorService = executorService;
-        this.delegate = delegate;
+        this.handler = handler;
     }
 
     @Override
     protected void doHandleRequest(Request request, HttpSession session) throws RequestHandlingException {
         try {
-            this.executorService.execute(() -> {
-                try {
-                    delegate.handleRequest(request, session);
-                } catch (IOException e) {
-                    handleInternalError(session, e);
-                }
-            });
+            this.executorService.execute(() -> delegateRequest(request, session));
         } catch (RejectedExecutionException e) {
-            try {
-                session.sendResponse(new Response(Response.SERVICE_UNAVAILABLE));
-            } catch (IOException ie) {
-                throw new RequestHandlingException(ie);
-            }
+            handleRejectedExecutionException(session);
+        }
+    }
+
+    private void delegateRequest(Request request, HttpSession session) {
+        try {
+            handler.handleRequest(request, session);
+        } catch (IOException e) {
+            handleInternalError(session, e);
+        }
+    }
+
+    private void handleRejectedExecutionException(HttpSession session) throws RequestHandlingException {
+        try {
+            session.sendResponse(new Response(Response.SERVICE_UNAVAILABLE));
+        } catch (IOException ie) {
+            throw new RequestHandlingException(ie);
         }
     }
 }
