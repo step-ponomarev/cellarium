af2b975169d26b6d0cafb84524b84446e50e2655
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2023-03-26 16:30:40 +0300

    changed

--- src/main/java/cellarium/http/handlers/ExecutorRequestHandler.java
+++ src/main/java/cellarium/http/handlers/ExecutorRequestHandler.java
@@ -2,6 +2,7 @@ package cellarium.http.handlers;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionException;
 import one.nio.http.HttpSession;
 import one.nio.http.Request;
 import one.nio.http.RequestHandler;
@@ -18,21 +19,20 @@ public final class ExecutorRequestHandler extends ARequestHandler {
 
     @Override
     protected void doHandleRequest(Request request, HttpSession session) throws RequestHandlingException {
-        if (executorService.isShutdown()) {
+        try {
+            this.executorService.execute(() -> {
+                try {
+                    delegate.handleRequest(request, session);
+                } catch (IOException e) {
+                    handleInternalError(session, e);
+                }
+            });
+        } catch (RejectedExecutionException e) {
             try {
                 session.sendResponse(new Response(Response.SERVICE_UNAVAILABLE));
-                return;
-            } catch (IOException e) {
-                throw new RequestHandlingException(e);
+            } catch (IOException ie) {
+                throw new RequestHandlingException(ie);
             }
         }
-        
-        this.executorService.execute(() -> {
-            try {
-                delegate.handleRequest(request, session);
-            } catch (IOException e) {
-                handleInternalError(session, e);
-            }
-        });
     }
 }
