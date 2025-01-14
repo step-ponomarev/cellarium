50a3e53b6a3f2166e98a7cf02640b075a93bb827
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2025-01-14 18:44:43 +0300

    refactored TombstoneFilterIterator

--- db/src/main/java/cellarium/db/database/iterators/TombstoneFilterIterator.java
+++ db/src/main/java/cellarium/db/database/iterators/TombstoneFilterIterator.java
@@ -1,41 +1,48 @@
 package cellarium.db.database.iterators;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import cellarium.db.database.table.TableRow;
 
 public final class TombstoneFilterIterator<T extends TableRow<?>> implements Iterator<T> {
     private final Iterator<T> iterator;
     private T nextValue;
+    private boolean hasNext;
 
     public TombstoneFilterIterator(Iterator<T> iterator) {
         this.iterator = iterator;
         this.nextValue = null;
-        prepareNextValue();
     }
 
     @Override
     public boolean hasNext() {
-        return nextValue != null;
+        if (hasNext) {
+            return true;
+        }
+
+        while (iterator.hasNext()) {
+            nextValue = iterator.next();
+            if (nextValue.getColumns() == null) {
+                continue;
+            }
+
+            hasNext = true;
+            break;
+        }
+
+        return hasNext;
     }
 
     @Override
     public T next() {
-        final T next = this.nextValue;
-        prepareNextValue();
-
-
-        return next;
-    }
-
-    private void prepareNextValue() {
-        while (this.iterator.hasNext()) {
-            nextValue = this.iterator.next();
-            if (nextValue.getColumns() != null) {
-                return;
-            }
+        if (!hasNext()) {
+            throw new NoSuchElementException();
         }
 
-        nextValue = null;
+        hasNext = false;
+
+        return this.nextValue;
     }
+
 }
